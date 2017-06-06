82e855e8126a1542520eb247dcd604371f14b13b
Tony Wang <sspy45@gmail.com>
2017-06-06 11:29:12 -0700

    added face detection and canny

--- frontend/components/take_image.jsx
+++ frontend/components/take_image.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import jsfeat from 'jsfeat';
+import profiler from './canny/profiler';
 import { faceClassifier } from './canny/face_classifier';
 // import { test } from './canny/test';
 export default class TakeImage extends React.Component {
@@ -7,6 +8,8 @@ export default class TakeImage extends React.Component {
   componentDidMount(){
     // Grab elements, create settings, etc.
     let canvas = document.getElementById('canvas-pic');
+    let canvasW = canvas.width;
+    let canvasH = canvas.height;
     let video = document.getElementById('video');
     let context = canvas.getContext('2d');
     // Get access to the camera!
@@ -18,7 +21,7 @@ export default class TakeImage extends React.Component {
         });
     }
     document.getElementById("snap").addEventListener("click", function() {
-    	context.drawImage(video, 0, 0, 480, 360);
+    	context.drawImage(video, 0, 0, canvasW, canvasH);
       //Copies the picture canvas translates to the calculation canvas
       var calcCanvas = document.getElementById('calcCanvas');
       var calcCtx = calcCanvas.getContext('2d');
@@ -68,24 +71,77 @@ export default class TakeImage extends React.Component {
         options.scale_factor,
         options.min_scale
       );
-      rects = jsfeat.haar.group_rectangles(rects, 1);
-      // draw only most confident one
-      draw_faces(calcCtx, rects, 480/img_u8.cols, 1);
-    });
 
-    function draw_faces(ctx, rects, sc, max) {
+      // draw only most confident one
+      rects = jsfeat.haar.group_rectangles(rects, 1);
+      let scale = w / img_u8.cols;
+      let face = getFace(calcCtx, rects, scale, 1);
+
+      // apply filter to canvas;
+      applyCanny(calcCtx, w, h, img_u8, options);
+      drawFace(calcCtx, face, scale);
+
+    });
+    let stat = new profiler();
+    stat.add("grayscale");
+    stat.add("gauss blur");
+    stat.add("canny edge");
+
+    function applyCanny(ctx, w, h, img_u8, options){
+      stat.new_frame();
+      ctx.drawImage(video, 0, 0, canvasW, canvasH);
+      var imageData = ctx.getImageData(0, 0, canvasW, canvasH);
+
+      stat.start("grayscale");
+      jsfeat.imgproc.grayscale(imageData.data, canvasW, canvasH, img_u8);
+      stat.stop("grayscale");
+
+      var r = options.blur_radius|0;
+      var kernel_size = (r+1) << 1;
+
+      stat.start("gauss blur");
+      jsfeat.imgproc.gaussian_blur(img_u8, img_u8, kernel_size, 0);
+      stat.stop("gauss blur");
+
+      stat.start("canny edge");
+      jsfeat.imgproc.canny(img_u8, img_u8, options.low_threshold|0, options.high_threshold|0);
+      stat.stop("canny edge");
+
+      // render result back to canvas
+      var data_u32 = new Uint32Array(imageData.data.buffer);
+      var alpha = (0xff << 24);
+      var i = img_u8.cols*img_u8.rows, pix = 0;
+      while(--i >= 0) {
+          pix = img_u8.data[i];
+          data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+      console.log(img_u8);
+    }
+
+    function getFace(ctx, rects, sc, max) {
       var on = rects.length;
       if(on && max) {
-        jsfeat.math.qsort(rects, 0, on-1, function(a,b){return (b.confidence<a.confidence);})
+        jsfeat.math.qsort(rects, 0, on-1, function(a,b){
+          return (b.confidence<a.confidence);
+        });
       }
       var n = max || on;
       n = Math.min(n, on);
       var r;
       for(var i = 0; i < n; ++i) {
           r = rects[i];
-          ctx.strokeRect((r.x*sc)|0,(r.y*sc)|0,(r.width*sc)|0,(r.height*sc)|0);
+
       }
+      return r;
     }
+
+    function drawFace(ctx, r, sc){
+      ctx.strokeStyle="red";
+      ctx.strokeRect((r.x*sc)|0,(r.y*sc)|0,(r.width*sc)|0,(r.height*sc)|0);
+    }
+
   }
 
 
