15c9c7636fc3f98b60c6b792495c87c6266a45c2
Samtfm <sfmemail@gmail.com>
2017-06-05 18:12:40 -0700

    group svgs and redraw

--- frontend/components/shirt.jsx
+++ frontend/components/shirt.jsx
@@ -5,29 +5,55 @@ class Shirt extends React.Component{
 
   componentDidMount(){
     let i = 100;
-    this.drawShirt(i);
 
+    const draw = SVG(this.drawing).size(300,300);
+    let last = this.drawShirt(draw, i);
+    setInterval(() => {
+      i += 10;
+      if (i< 200){
+        last = last.replace(this.drawShirt(draw, i));
+
+      }
+
+    }, 200);
   }
 
-  drawShirt(){
+  drawShirt(draw, width){
     // set up draw constant for svg.js
-   const draw = SVG(this.drawing).size(300,300);
 
-   //draw a silly line and circle
-   const line1 = draw.polyline('50,20 100,50 140,30').fill('none').stroke({ width: 1 });
-   const circle = draw.circle('30');
+    // create points based on measurement
+    const points = [
+     [width, 50],
+     [width*.8, 100],
+     [100, 150]
+    ];
 
-   //find first vector point from line1
-   const firstPoint = line1.node.points[0];
+    // boil points into a happy little string "100,0 80,50 110,100"
+    const pointString = points.map(pair => (
+     `${pair[0].toString()}, ${pair[1].toString()}`
+    )).join(' ');
 
-   // position circle at that point ( cx is circlex i think?)
-   circle.attr({cx: firstPoint.x, cy: firstPoint.y });
+    // draw line from points
+    const line1 = draw.polyline(pointString).fill('none').stroke({ width: 1 });
+
+    // draw silly circle at the head of theline
+    const circle = draw.circle('30');
+    //find first vector point from line1
+    const firstPoint = line1.node.points[0];
+    // position circle at that point ( cx is circlex i think?)
+    circle.attr({cx: firstPoint.x, cy: firstPoint.y });
+
+    // important! group objects into single svg component
+    // this allows it to be returned and then replaced
+    const group = draw.group();
+    group.add(circle);
+    group.add(line1);
+    return group;
   }
 
   render(){
     return(
-      <div id="drawing"
-        ref={(div) => { this.drawing = div;}}></div>
+      <div ref={(div) => { this.drawing = div;}}></div>
     );
   }
 }
