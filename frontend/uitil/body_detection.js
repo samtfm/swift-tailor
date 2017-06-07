c2fa060da89c620c9280f5b033f8d701802ce741
Samtfm <sfmemail@gmail.com>
2017-06-06 17:35:21 -0700

    clean up detectOutlinePoints

--- frontend/uitil/body_detection.js
+++ frontend/uitil/body_detection.js
@@ -1,24 +1,24 @@
 export const detectOutlinePoints = imageData => {
   const rows = imageData.rows;
-  const cols= imageData.cols;
+  const cols = imageData.cols;
 
   const points = [];
-  for (let y = 0; y < imageData.cols; y++) {
+  for (let y = 0; y < rows; y++) {
     //slice from start of collumn to end of row
     const column = imageData.data.slice(rows*cols, rows*cols+rows);
-
+    const mid = Math.floor(column.length/2);
 
     let leftEdge = 0;
     let rightEdge = 0;
     //check right edge
-    for(let x = column.length/2; x < column.length; x++) {
+    for(let x = mid; x < cols; x++) {
       const value = imageData.data[x];
       if (value > 0) {
         rightEdge = x;
       }
     }
     //check for left edge
-    for(let x = column.length/2; x > 0; x--) {
+    for(let x = mid; x > 0; x--) {
       const value = imageData.data[x];
       if (value > 0) {
         leftEdge = x; // reasign until edge of frame
