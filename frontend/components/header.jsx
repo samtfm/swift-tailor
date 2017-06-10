71cf317da7b6ac8e4647d80cc408df4f91ca80e5
Samtfm <sfmemail@gmail.com>
2017-06-10 15:20:48 -0700

    set default font

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return(
-    <header>
+    <header className='logo'>
       <Link to="/">Tailor Swift</Link>
     </header>
   );
