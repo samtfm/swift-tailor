cd8a4a6f6a07180b9f17c87a7d3d9af5a1e952f3
Samtfm <sfmemail@gmail.com>
2017-06-10 09:45:33 -0700

    add state to template

--- frontend/components/template_editor.jsx
+++ frontend/components/template_editor.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 
 class TemplateEditor extends React.Component{
 
-  render(){
+  constructor(props){
+    super(props);
+    this.state = { height: 70, neck: 3, chest: 14, waist: 16 };
+  }
 
+  updateValue(e){
+    console.log(e.target);
+  }
+
+  render(){
+    const { height, neck, chest, waist } = this.state;
     return (
-      <input type = 'text' value='72' onChange=></input>
-      <ul>
-        <li>height: {height}</li>
-        <li>neck: {neck}</li>
-        <li>chest: {chest}</li>
-        <li>waist: {waist}</li>
-      </ul>
-      <Shirt measurements={{}/*this.state.measurements*/} />
-    )
+      <div>
+        <ul>
+          <input type = 'text' value='72' onChange={this.updateValue.bind(this)}></input>
+          <li>height: {height}</li>
+          <li>neck: {neck}</li>
+          <li>chest: {chest}</li>
+          <li>waist: {waist}</li>
+        </ul>
+        <Shirt measurements={{}/*this.state.measurements*/} />
+      </div>
+    );
   }
 
 }
