95680368b690173bb9226ca0cd849049ec992716
Samtfm <sfmemail@gmail.com>
2017-06-10 12:15:19 -0700

    move inch measurements to templateEditor

--- frontend/components/template_editor.jsx
+++ frontend/components/template_editor.jsx
@@ -5,14 +5,42 @@ class TemplateEditor extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = { height: 70, neck: 3, chest: 14, waist: 16 };
+    this.state = {
+      inputs: { height: 70, neck: 3, chest: 14, waist: 16 },
+      inchMeasurements: {}
+    };
+    this.updateValue = this.updateValue.bind(this);
   }
 
   updateValue(e){
-    const newState = {};
-    newState[e.target.name] = e.target.value;
+    const newState = { inputs: {} };
+    newState.inputs[e.target.name] = e.target.value;
     this.setState(newState);
   }
+
+  componentWillReceiveProps(newProps){
+    if (!newProps.measurements || !newProps.measurements.arms) return;
+    let {arms, neck, chest, waist } = newProps.measurements;
+    const heightInches = 71;
+    const rawHeight = arms.wingspan * 0.98;
+    const scaleFactor = heightInches/rawHeight;
+    const chestWidth = chest.average * scaleFactor;
+    const waistWidth = waist.maximum * scaleFactor;
+    const shirtLength = arms.wingspan * 0.4 * scaleFactor;
+    const shoulderWidth = chest.average * 0.9 * scaleFactor;
+    const neckWidth = neck.mininum * scaleFactor;
+
+    const armHole = shirtLength*.25;
+    this.setState({
+      inchMeasurements: {
+        height: heightInches,
+        neck: neckWidth,
+        chest: chestWidth,
+        waist: waistWidth
+      }
+   });
+  }
+
 
   render(){
     const { height, neck, chest, waist } = this.state;
@@ -21,22 +49,24 @@ class TemplateEditor extends React.Component{
         <ul>
           <label>
             Height:
-            <input type = 'text' name='height' value={height} onChange={this.updateValue.bind(this)}></input>
+            <input type = 'text' name='height' value={height} onChange={this.updateValue}></input>
           </label>
           <label>
             Neck:
-            <input type = 'text' name='neck' value={neck} onChange={this.updateValue.bind(this)}></input>
+            <input type = 'text' name='neck' value={neck} onChange={this.updateValue}></input>
           </label>
           <label>
             Chest:
-            <input type = 'text' name='chest' value={chest} onChange={this.updateValue.bind(this)}></input>
+            <input type = 'text' name='chest' value={chest} onChange={this.updateValue}></input>
           </label>
           <label>
             Waist:
-            <input type = 'text' name='waist' value={waist} onChange={this.updateValue.bind(this)}></input>
+            <input type = 'text' name='waist' value={waist} onChange={this.updateValue}></input>
           </label>
         </ul>
-        <Shirt measurements={{}/*this.state.measurements*/} />
+        <Shirt
+          pixelMeasurements={this.state.pixelMeasurements /*this.state.measurements*/}
+          inchMeasurements={this.state.inchMeasurements} />
       </div>
     );
   }
